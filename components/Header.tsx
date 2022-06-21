34e29d052a39169e51051a7e6ea52f72a4158649
Cathal Mullan <contact@cathal.dev>
2022-06-21 21:19:50 +0100

    Use AWS OIDC for deployment

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import GitHubLink from 'components/links/GitHubLink'
 import LinkedInLink from 'components/links/LinkedInLink'
 import EmailLink from './links/EmailLink'
+import LightningBolt from 'public/svg/LightningBolt.svg'
 
 export default function Header() {
   return (
@@ -9,7 +10,8 @@ export default function Header() {
       <div className="container mx-auto flex h-16 max-w-4xl items-center justify-between px-6 py-4">
         <Link href="/">
           <a className="flex items-center p-2 leading-none">
-            <span className="text-lg font-bold">cathal.dev</span>
+            <LightningBolt className="h-10 w-10 pr-2 text-amber-400" />
+            <span className="text-xl font-bold">cathal.dev</span>
           </a>
         </Link>
 
