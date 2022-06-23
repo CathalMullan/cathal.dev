2eded90bb821a55a67a54037d2bc99f1129a32ed
Cathal Mullan <contact@cathal.dev>
2022-06-23 21:13:32 +0100

    Less distracting border on header/footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import SitemapLink from './links/SitemapLink'
 
 export default function Footer() {
   return (
-    <footer className="mt-12 w-full border-t bg-white pt-10 pb-10 text-sm dark:bg-[#111b27]">
+    <footer className="mt-12 w-full border-t border-gray-2 dark:border-gray-600 bg-white pt-10 pb-10 text-sm dark:bg-[#111b27]">
       <div className="flex justify-center">Copyright © 2022 Cathal Mullan</div>
 
       <div className="flex justify-center space-x-4 py-2">
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import ThemeToggle from './ThemeToggle'
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 w-full border-b bg-white dark:bg-[#111b27]">
+    <header className="sticky top-0 z-10 w-full border-b border-gray-200 dark:border-gray-600 bg-white dark:bg-[#111b27]">
       <div className="container mx-auto flex h-16 items-center justify-between px-6 py-4">
         <Link href="/">
           <a className="flex items-center p-2">
