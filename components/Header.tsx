7850e867341857842cb4297ba819653d46cfe846
Cathal Mullan <contact@cathal.dev>
2022-07-02 20:24:03 +0100

    Improve A11Y support on buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ export function Header() {
     <header className="w-full border-b border-gray-200 pt-5 pb-5 dark:border-gray-600 dark:bg-code_dark">
       <div className="flex items-center justify-between">
         <Link href="/">
-          <button type="submit" className="flex items-center space-x-2 pl-8">
+          <button type="submit" aria-label="Home" className="flex items-center space-x-2 pl-8">
             <LightningBolt className="h-8 w-8 text-amber-400" />
             <span className="text-xl font-bold">cathal.dev</span>
           </button>
--- components/links/SearchLink.tsx
+++ components/links/SearchLink.tsx
@@ -4,7 +4,7 @@ import SearchIcon from "public/svg/SearchIcon.svg";
 export function SearchLink() {
   return (
     <Link href="/search">
-      <button type="submit">
+      <button type="submit" aria-label="Search">
         <SearchIcon className="h-5 w-5" />
       </button>
     </Link>
