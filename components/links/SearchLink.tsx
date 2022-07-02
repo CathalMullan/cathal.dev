1f96a285a21b1b3384d41799ba53c9320085883b
Cathal Mullan <contact@cathal.dev>
2022-07-02 20:19:20 +0100

    Correct "<Link> ref error

--- components/links/SearchLink.tsx
+++ components/links/SearchLink.tsx
@@ -4,7 +4,9 @@ import SearchIcon from "public/svg/SearchIcon.svg";
 export function SearchLink() {
   return (
     <Link href="/search">
-      <SearchIcon className="h-5 w-5" />
+      <button type="submit">
+        <SearchIcon className="h-5 w-5" />
+      </button>
     </Link>
   );
 }
